02d5e60be6ed3399a7ff981bd4782a0285f8e606
K4r0x <deox846@gmail.com>
2026-04-18 18:21:50 +0200

    adc??

--- nRF54L15/src/main.c
+++ nRF54L15/src/main.c
@@ -3,6 +3,7 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/adc.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/adc.h>
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
@@ -18,17 +19,33 @@ int main(void)
     /* Pobieramy podzespoly z naszego pliku overlay */
     const struct device *const bme280_dev = DEVICE_DT_GET_ANY(bosch_bme280);
     const struct device *const vl53l0x_dev = DEVICE_DT_GET_ANY(st_vl53l0x);
+    static const struct adc_dt_spec adc_channel = ADC_DT_SPEC_GET(DT_PATH(zephyr_user));
 
-    if (bme280_dev == NULL || !device_is_ready(bme280_dev)) {
+    if (bme280_dev == NULL || !device_is_ready(bme280_dev))
+    {
         LOG_ERR("Blad! Nie mozna zainicjowac BME280.");
-    } else {
+    }
+    else
+    {
         LOG_INF("Sukces! BME280 gotowy.");
     }
 
-    if (vl53l0x_dev == NULL || !device_is_ready(vl53l0x_dev)) {
+    if (vl53l0x_dev == NULL || !device_is_ready(vl53l0x_dev))
+    {
         LOG_ERR("Blad! Nie mozna zainicjowac VL53L0X. Sprawdz piny P1.12, P1.13.");
-    } else {
+    }
+    else
+    {
         LOG_INF("Sukces! VL53L0X gotowy.");
+    }
+
+    if (!adc_is_ready_dt(&adc_channel))
+    {
+        LOG_ERR("Blad! Kontroler ADC nie jest gotowy.");
+    }
+    else
+    {
+        LOG_INF("Sukces! ADC gotowe.");
     }
 
     LOG_INF("Rozpoczynam pomiary...\n");
@@ -36,28 +53,26 @@ int main(void)
     struct sensor_value temp, press, humidity;
     struct sensor_value distance;
 
-    /* Konfiguracja ADC */
-    int16_t adc_sample_buffer;
-    struct adc_sequence adc_seq = {
-        .buffer = &adc_sample_buffer,
-        .buffer_size = sizeof(adc_sample_buffer),
-    };
+    int err;
+    int16_t sample_buffer;
 
-    if (!adc_is_ready_dt(&adc_channel)) {
-        LOG_ERR("Blad! Urzadzenie ADC nie gotowe.");
+    struct adc_sequence sequence = {
+        .buffer = &sample_buffer,
+        .buffer_size = sizeof(sample_buffer),
+    };
+    err = adc_channel_setup_dt(&adc_channel);
+    if (err < 0) {
+        LOG_ERR("Blad konfiguracji kanalu ADC: %d", err);
     } else {
-        int err = adc_channel_setup_dt(&adc_channel);
-        if (err < 0) {
-            LOG_ERR("Nie mozna skonfigurowac kanalu ADC: %d", err);
-        } else {
-            LOG_INF("Sukces! Kanal ADC skonfigurowany.");
-        }
-        adc_sequence_init_dt(&adc_channel, &adc_seq);
+        LOG_INF("Kanal ADC skonfigurowany pomyslnie.");
     }
+    adc_sequence_init_dt(&adc_channel, &sequence);
 
     /* Nieskończona pętla odczytująca dane co 2 sekundy */
-    while (1) {
-        if (device_is_ready(bme280_dev)) {
+    while (1)
+    {
+        if (device_is_ready(bme280_dev))
+        {
             /* Pobranie nowej próbki z czujnika */
             sensor_sample_fetch(bme280_dev);
 
@@ -73,27 +88,25 @@ int main(void)
                     sensor_value_to_double(&humidity));
         }
 
-        if (device_is_ready(vl53l0x_dev)) {
+        if (device_is_ready(vl53l0x_dev))
+        {
             sensor_sample_fetch(vl53l0x_dev);
             sensor_channel_get(vl53l0x_dev, SENSOR_CHAN_DISTANCE, &distance);
             LOG_INF("Dystans VL53L0X: %.3f m", sensor_value_to_double(&distance));
         }
 
-        if (adc_is_ready_dt(&adc_channel)) {
-            int err = adc_read_dt(&adc_channel, &adc_seq);
-            if (err == 0) {
-                int32_t val_mv = adc_sample_buffer;
-                LOG_INF("ADC: wartosc RAW: %d", adc_sample_buffer);
-                
-                adc_raw_to_millivolts_dt(&adc_channel, &val_mv);
-                LOG_INF("ADC: %.3f V", val_mv / 1000.0); // albo mV
-            } else {
-                LOG_ERR("Blad pomiaru ADC: %d", err);
-            }
+        err = adc_read_dt(&adc_channel, &sequence);
+        if (err == 0)
+        {
+            LOG_INF("Wartosc napiecia (szurowe ADC): %d", sample_buffer);
+        }
+        else
+        {
+            LOG_ERR("Blad pomiaru ADC: %d", err);
         }
 
         k_sleep(K_MSEC(100)); // Czekamy 100 ms przed kolejnym pomiarem
     }
-    
+
     return 0;
 }
