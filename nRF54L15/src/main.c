eb8aaef8595bb950765323a3b76fd2918e67968e
domin746826 <ominek.pl91@gmail.com>
2026-04-18 17:34:50 +0200

    n

--- nRF54L15/src/main.c
+++ nRF54L15/src/main.c
@@ -1,13 +1,21 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/adc.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
+#if !DT_NODE_EXISTS(DT_PATH(zephyr_user)) || \
+    !DT_NODE_HAS_PROP(DT_PATH(zephyr_user), io_channels)
+#error "Brak zdefiniowanego zephyr,user io-channels dla ADC w pliku .overlay!"
+#endif
+
+static const struct adc_dt_spec adc_channel = ADC_DT_SPEC_GET(DT_PATH(zephyr_user));
+
 int main(void)
 {
-    /* Pobieramy czujnik BME280 z naszego pliku overlay */
+    /* Pobieramy podzespoly z naszego pliku overlay */
     const struct device *const bme280_dev = DEVICE_DT_GET_ANY(bosch_bme280);
     const struct device *const vl53l0x_dev = DEVICE_DT_GET_ANY(st_vl53l0x);
 
@@ -27,6 +35,25 @@ int main(void)
 
     struct sensor_value temp, press, humidity;
     struct sensor_value distance;
+
+    /* Konfiguracja ADC */
+    int16_t adc_sample_buffer;
+    struct adc_sequence adc_seq = {
+        .buffer = &adc_sample_buffer,
+        .buffer_size = sizeof(adc_sample_buffer),
+    };
+
+    if (!adc_is_ready_dt(&adc_channel)) {
+        LOG_ERR("Blad! Urzadzenie ADC nie gotowe.");
+    } else {
+        int err = adc_channel_setup_dt(&adc_channel);
+        if (err < 0) {
+            LOG_ERR("Nie mozna skonfigurowac kanalu ADC: %d", err);
+        } else {
+            LOG_INF("Sukces! Kanal ADC skonfigurowany.");
+        }
+        adc_sequence_init_dt(&adc_channel, &adc_seq);
+    }
 
     /* Nieskończona pętla odczytująca dane co 2 sekundy */
     while (1) {
@@ -50,6 +77,19 @@ int main(void)
             sensor_sample_fetch(vl53l0x_dev);
             sensor_channel_get(vl53l0x_dev, SENSOR_CHAN_DISTANCE, &distance);
             LOG_INF("Dystans VL53L0X: %.3f m", sensor_value_to_double(&distance));
+        }
+
+        if (adc_is_ready_dt(&adc_channel)) {
+            int err = adc_read_dt(&adc_channel, &adc_seq);
+            if (err == 0) {
+                int32_t val_mv = adc_sample_buffer;
+                LOG_INF("ADC: wartosc RAW: %d", adc_sample_buffer);
+                
+                adc_raw_to_millivolts_dt(&adc_channel, &val_mv);
+                LOG_INF("ADC: %.3f V", val_mv / 1000.0); // albo mV
+            } else {
+                LOG_ERR("Blad pomiaru ADC: %d", err);
+            }
         }
 
         k_sleep(K_MSEC(100)); // Czekamy 100 ms przed kolejnym pomiarem
