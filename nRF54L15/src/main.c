8f2f4e7ee42f357d35b65a92cd671a171de96af4
K4r0x <deox846@gmail.com>
2026-04-19 03:11:29 +0200

    NFC+MAC+SENSORS_REQUEST

--- nRF54L15/src/main.c
+++ nRF54L15/src/main.c
@@ -10,11 +10,22 @@
 #include <zephyr/bluetooth/uuid.h>
 #include <zephyr/bluetooth/gatt.h>
 #include <zephyr/sys/util.h>
+#include <stdio.h>
+#include <string.h>
 
 #include <nfc_t2t_lib.h>
 #include <nfc/ndef/uri_msg.h>
 
 LOG_MODULE_REGISTER(main_app, LOG_LEVEL_INF);
+
+#define SENSOR_LOG_INF(...)                 \
+    do                                      \
+    {                                       \
+        if (IS_ENABLED(CONFIG_LOG_SENSORS)) \
+        {                                   \
+            LOG_INF(__VA_ARGS__);           \
+        }                                   \
+    } while (0)
 
 /* Sensory */
 const struct device *const bme280 = DEVICE_DT_GET_ANY(bosch_bme280);
@@ -47,14 +58,21 @@ static uint32_t last_pressure_pa = 0;
 
 #define NDEF_MSG_BUF_SIZE 256
 static uint8_t ndef_msg_buf[NDEF_MSG_BUF_SIZE];
-static const uint8_t nfc_pair_url[] =
-    "http://heimdall.local/api/pair?mac=A1:B2:C3:D4:E5:F6";
+#define NFC_PAIR_URL_BASE "http://heimdall.local/api/pair"
+static char ble_mac_str[BT_ADDR_LE_STR_LEN] = "00:00:00:00:00:00";
+static char nfc_pair_url[256];
 
 static bool notify_distance_enabled;
 static bool notify_temp_enabled;
 static bool notify_humidity_enabled;
 static bool notify_pressure_enabled;
 static bool notify_light_enabled;
+
+static bool sensor_distance_available;
+static bool sensor_temp_available;
+static bool sensor_humidity_available;
+static bool sensor_pressure_available;
+static bool sensor_light_available;
 
 static const struct adc_dt_spec light_adc = ADC_DT_SPEC_GET_BY_IDX(DT_PATH(zephyr_user), 0);
 static int16_t light_sample_buffer;
@@ -140,21 +158,101 @@ static void nfc_callback(void *context, nfc_t2t_event_t event,
     }
 }
 
-static int nfc_init_url_tag(void)
+static void append_sensor_name(char *buffer, size_t buffer_size,
+                               const char *name, bool *first)
+{
+    size_t used = strlen(buffer);
+
+    if (used >= buffer_size - 1)
+    {
+        return;
+    }
+
+    if (!(*first))
+    {
+        snprintf(buffer + used, buffer_size - used, ",");
+        used = strlen(buffer);
+        if (used >= buffer_size - 1)
+        {
+            return;
+        }
+    }
+
+    snprintf(buffer + used, buffer_size - used, "%s", name);
+    *first = false;
+}
+
+static void build_nfc_pair_url_from_available_sensors(void)
+{
+    char sensors_param[96] = {0};
+    bool first = true;
+
+    if (sensor_distance_available)
+    {
+        append_sensor_name(sensors_param, sizeof(sensors_param), "distance", &first);
+    }
+    if (sensor_temp_available)
+    {
+        append_sensor_name(sensors_param, sizeof(sensors_param), "temp", &first);
+    }
+    if (sensor_humidity_available)
+    {
+        append_sensor_name(sensors_param, sizeof(sensors_param), "humidity", &first);
+    }
+    if (sensor_pressure_available)
+    {
+        append_sensor_name(sensors_param, sizeof(sensors_param), "pressure", &first);
+    }
+    if (sensor_light_available)
+    {
+        append_sensor_name(sensors_param, sizeof(sensors_param), "light", &first);
+    }
+
+    if (first)
+    {
+        snprintf(nfc_pair_url, sizeof(nfc_pair_url), "%s?mac=%s&sensors=none",
+                 NFC_PAIR_URL_BASE, ble_mac_str);
+    }
+    else
+    {
+        snprintf(nfc_pair_url, sizeof(nfc_pair_url), "%s?mac=%s&sensors=%s",
+                 NFC_PAIR_URL_BASE, ble_mac_str, sensors_param);
+    }
+}
+
+static void update_local_ble_mac(void)
+{
+    bt_addr_le_t addrs[CONFIG_BT_ID_MAX];
+    size_t count = ARRAY_SIZE(addrs);
+    char addr_le_str[BT_ADDR_LE_STR_LEN];
+    char *type_sep;
+
+    bt_id_get(addrs, &count);
+    if (count == 0)
+    {
+        LOG_ERR("Cannot read local BLE identity address");
+        return;
+    }
+
+    bt_addr_le_to_str(&addrs[0], addr_le_str, sizeof(addr_le_str));
+    type_sep = strchr(addr_le_str, ' ');
+    if (type_sep)
+    {
+        *type_sep = '\0';
+    }
+
+    snprintf(ble_mac_str, sizeof(ble_mac_str), "%s", addr_le_str);
+    LOG_INF("Local BLE MAC: %s", ble_mac_str);
+}
+
+static int nfc_update_url_payload(void)
 {
     int err;
     uint32_t len = sizeof(ndef_msg_buf);
 
-    err = nfc_t2t_setup(nfc_callback, NULL);
-    if (err)
-    {
-        LOG_ERR("NFC setup failed (%d)", err);
-        return err;
-    }
-
     err = nfc_ndef_uri_msg_encode(NFC_URI_NONE,
-                                  nfc_pair_url,
-                                  sizeof(nfc_pair_url) - 1,
+                                  (const uint8_t *)nfc_pair_url,
+                                  strlen(nfc_pair_url),
                                   ndef_msg_buf,
                                   &len);
     if (err)
@@ -170,6 +268,26 @@ static int nfc_init_url_tag(void)
         return err;
     }
 
+    return 0;
+}
+
+static int nfc_init_url_tag(void)
+{
+    int err;
+
+    err = nfc_t2t_setup(nfc_callback, NULL);
+    if (err)
+    {
+        LOG_ERR("NFC setup failed (%d)", err);
+        return err;
+    }
+
+    err = nfc_update_url_payload();
+    if (err)
+    {
+        return err;
+    }
+
     err = nfc_t2t_emulation_start();
     if (err)
     {
@@ -179,6 +297,24 @@ static int nfc_init_url_tag(void)
 
     LOG_INF("NFC URI ready: %s", nfc_pair_url);
     return 0;
+}
+
+static void detect_and_store_available_sensors(bool adc_ready)
+{
+    sensor_distance_available = device_is_ready(vl53l0x);
+
+    sensor_temp_available = device_is_ready(bme280);
+    sensor_humidity_available = sensor_temp_available;
+    sensor_pressure_available = sensor_temp_available;
+
+    sensor_light_available = adc_ready;
+
+    SENSOR_LOG_INF("Sensors available: distance=%d temp=%d humidity=%d pressure=%d light=%d",
+                   sensor_distance_available,
+                   sensor_temp_available,
+                   sensor_humidity_available,
+                   sensor_pressure_available,
+                   sensor_light_available);
 }
 
 /* Definicja serwisu GATT */
@@ -253,14 +389,9 @@ int main(void)
         .buffer = &light_sample_buffer,
         .buffer_size = sizeof(light_sample_buffer),
     };
+    bool adc_ready = false;
 
     LOG_INF("Starting Bluetooth Sensor App on nRF54L15");
-
-    err = nfc_init_url_tag();
-    if (err)
-    {
-        LOG_ERR("NFC init error (%d), BLE sensor app will continue", err);
-    }
 
     /* Inicjalizacja BT */
     err = bt_enable(bt_ready);
@@ -269,6 +400,8 @@ int main(void)
         LOG_ERR("Bluetooth enable failed (err %d)", err);
         return 0;
     }
+
+    update_local_ble_mac();
 
     /* Sprawdzamy czy laser działa (nie zatrzymujemy programu jeśli nie, żeby BT działało) */
     if (!device_is_ready(vl53l0x))
@@ -287,15 +420,26 @@ int main(void)
     }
     else
     {
+        adc_ready = true;
         ret = adc_channel_setup_dt(&light_adc);
         if (ret < 0)
         {
             LOG_ERR("Blad konfiguracji ADC kanału: %d", ret);
+            adc_ready = false;
         }
 
         light_sequence.channels = BIT(light_adc.channel_id);
         light_sequence.resolution = light_adc.resolution;
         light_sequence.oversampling = light_adc.oversampling;
+    }
+
+    detect_and_store_available_sensors(adc_ready);
+    build_nfc_pair_url_from_available_sensors();
+
+    err = nfc_init_url_tag();
+    if (err)
+    {
+        LOG_ERR("NFC init error (%d), BLE sensor app will continue", err);
     }
 
     while (1)
@@ -306,7 +450,7 @@ int main(void)
             sensor_channel_get(vl53l0x, SENSOR_CHAN_DISTANCE, &distance);
 
             last_distance_mm = (uint16_t)(distance.val1 * 1000 + distance.val2 / 1000);
-            LOG_INF("Distance: %d mm", last_distance_mm);
+            SENSOR_LOG_INF("Distance: %d mm", last_distance_mm);
 
             /* Wypychamy powiadomienie (Notify) do połączonych urządzeń */
             if (notify_distance_enabled)
@@ -327,8 +471,8 @@ int main(void)
             last_humidity_centi = (uint16_t)(humidity.val1 * 100 + humidity.val2 / 10000);
             last_pressure_pa = (uint32_t)(pressure.val1 * 1000 + pressure.val2 / 1000);
 
-            LOG_INF("Temp(cC): %d, Hum(c%%): %u, Press(Pa): %u",
-                    last_temp_centi, last_humidity_centi, last_pressure_pa);
+            SENSOR_LOG_INF("Temp(cC): %d, Hum(c%%): %u, Press(Pa): %u",
+                           last_temp_centi, last_humidity_centi, last_pressure_pa);
             if (notify_temp_enabled)
             {
                 bt_gatt_notify_uuid(NULL, &temp_char_uuid.uuid, &sensor_svc.attrs[0],
@@ -352,7 +496,7 @@ int main(void)
             if (ret == 0)
             {
                 last_light_raw = (uint16_t)light_sample_buffer;
-                LOG_INF("Light(raw): %u", last_light_raw);
+                SENSOR_LOG_INF("Light(raw): %u", last_light_raw);
                 if (notify_light_enabled)
                 {
                     bt_gatt_notify_uuid(NULL, &light_char_uuid.uuid, &sensor_svc.attrs[0],
